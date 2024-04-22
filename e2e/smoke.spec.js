3c10814f41d886189f6bf8a85be0daa4c6cca036
Bartosz Cwynar <bartosz.cwynar@yahoo.com>
2024-04-22 22:57:45 +0200

    process cwd

--- e2e/smoke.spec.js
+++ e2e/smoke.spec.js
@@ -2,8 +2,9 @@ const { _electron: electron } = require("playwright");
 const { test, expect } = require("@playwright/test");
 
 test("appStarts", async () => {
+  console.log(process.cwd());
   const electronApp = await electron.launch({
-    args: ["./dist/electron/UnPackaged/electron-main.js"],
+    args: [process.cwd() + "/dist/electron/UnPackaged/electron-main.js"],
   });
   const window = await electronApp.firstWindow();
   await electronApp.close();
