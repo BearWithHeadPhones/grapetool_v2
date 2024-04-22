36e390d3c6d93490baf2755d063fa5c8a50ac92c
Bartosz Cwynar <bartosz.cwynar@yahoo.com>
2024-04-22 23:26:28 +0200

    remove debug prints

--- e2e/smoke.spec.js
+++ e2e/smoke.spec.js
@@ -2,7 +2,6 @@ const { _electron: electron } = require("playwright");
 const { test, expect } = require("@playwright/test");
 
 test("appStarts", async () => {
-  console.log(process.cwd());
   const electronApp = await electron.launch({
     args: [process.cwd() + "/dist/electron/UnPackaged/electron-main.mjs"],
   });
