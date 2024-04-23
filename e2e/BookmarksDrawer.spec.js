c67854ea5cf04ec48e257bbad0892a2c769fe993
Bartosz Cwynar <bartosz.cwynar@yahoo.com>
2024-04-23 11:57:58 +0200

    bokmarksDrawer e2e

--- e2e/BookmarksDrawer.spec.js
+++ e2e/BookmarksDrawer.spec.js
@@ -1,13 +1,13 @@
 const { _electron: electron } = require("playwright");
 const { test, expect } = require("@playwright/test");
 
-test("close bookmarks drawer", async () => {
+test("toggle bookmarks drawer", async () => {
   const electronApp = await electron.launch({
     args: [process.cwd() + "/dist/electron/UnPackaged/electron-main.mjs"],
   });
   const window = await electronApp.firstWindow();
   await window.waitForLoadState();
   await window.click("#bookmarksToggle");
-  expect(window.locator("#bookmarksDrawer")).toBeHidden();
+  await expect(window.locator("#bookmarksDrawer")).toBeHidden();
   await electronApp.close();
 });
