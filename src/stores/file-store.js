35c4106d8468c3292b49d8cb9955cfd44407f8fe
Bartosz Cwynar <bartosz.cwynar@yahoo.com>
2024-04-23 22:13:55 +0200

    add sort

--- src/stores/file-store.js
+++ src/stores/file-store.js
@@ -1,6 +1,24 @@
 import { defineStore } from "pinia";
 import { useWorkSpaceStore } from "stores/workspace-store";
 
+function createWorkspace(content, sort) {
+  content = content.split("\n");
+  if (sort) {
+    content = content.sort();
+  }
+  let workspace = {
+    index: 0,
+    label: "root",
+    content: content.map((line, index) => {
+      return {
+        index: index + 1,
+        line: line.trim(),
+      };
+    }),
+  };
+  return workspace;
+}
+
 export const useFileStore = defineStore("fileStore", {
   state: () => ({
     originalFileContent: "",
@@ -14,24 +32,7 @@ export const useFileStore = defineStore("fileStore", {
       this.originalFileContent = content;
       const workSpaceStore = useWorkSpaceStore();
 
-      content = content.split("\n");
-      let sort = true;
-      if (sort) {
-        content = content.sort();
-      }
-      let workspace = {
-        index: 0,
-        label: "root",
-        content: content.map((line, index) => {
-          return {
-            index: index + 1,
-            line: line.trim(),
-          };
-        }),
-      };
-
-      workspace.content.sort();
-      workSpaceStore.setWorkspaces([workspace]);
+      workSpaceStore.setWorkspaces([createWorkspace(content, true)]);
 
       workSpaceStore.setCurrentWorkspaceIndex(0);
     },
